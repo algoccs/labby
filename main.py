e49f94af665ebde6959d18fe9f3c55ac6767512a
algoccs <algodemoacc@gmail.com>
2026-06-17 16:27:04 -0400

    Initialize Pygame labyrinth game structure

---
+++ main.py
@@ -0,0 +1,65 @@
+from pygame import *
+init()
+
+# CONSTANTES parametrs de inicio
+ANCHO, ALTO = 640, 480
+TITULO = 'Proyecto Laberinto'
+FPS = 60
+BACK_COLOR = (27, 80, 155)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+PLAYER_IMG = 'hero.png'
+ENEMY_IMG = 'cyborg.png'
+GOAL_IMG = 'trophy-1.png'
+
+# CLASE PRINCIPAL
+class GameSprite(sprite.Sprite):
+    def __init__(self, img_file, cor_x, cor_y, speed=0):
+        super().__init__()
+        self.image = transform.scale(image.load(img_file), (60, 60))
+        self.rect = self.image.get_rect()
+        self.rect.x = cor_x
+        self.rect.y = cor_y
+        self.speed = speed
+
+    def reset(self):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+
+        if keys[K_w] and self.rect.y >= 0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y <= ALTO - self.rect.height:
+            self.rect.y += self.speed
+        if keys[K_d] and self.rect.x <= ANCHO - self.rect.width:
+            self.rect.x += self.speed
+        if keys[K_a] and self.rect.x >= 0:
+            self.rect.x -= self.speed
+
+# PANTALLA (superficie principal)
+screen = display.set_mode((ANCHO, ALTO))
+display.set_caption(TITULO)
+clock = time.Clock()
+
+# OBJETOS
+player = Player(PLAYER_IMG, 20, 350, 5)
+enemy = GameSprite(ENEMY_IMG, 20, 350, 2)
+goal = GameSprite(GOAL_IMG, 20, 350, 2)
+
+
+# GAME LOOP
+run = True # variable de estado
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+
+    screen.fill(BACK_COLOR)
+    player.reset() # Metodo para renderizar la imagen
+    player.update() # METODO DE MOVIMIENTO
+
+    display.update()
+    clock.tick(FPS) # Establecer fotogramas por segundos
+quit()
